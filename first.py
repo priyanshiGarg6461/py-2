7dc8fa4b51b54f9ddd82bf73804d47b12e39ba4f
priyanshiGarg6461 <116329214+priyanshiGarg6461@users.noreply.github.com>
2022-10-25 12:37:09 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,19 +1,19 @@
 '''.printing every character entered by the user in new line'''
-# s=input('Enter the word: ')
-# str = s
-# for i in str:
-#     print(i)
+ s=input('Enter the word: ')
+ str = s
+for i in str:
+    print(i)
 
 
 '''Adding list of items in a set'''
-# set=set()
-# list=[]
-# a=int(input('Enter the number of items to add: '))
-# for i in range(1,a+1):
-#     z=input()
-#     list.append(z)
-# set.update(list)
-# print(set)
+set=set()
+list=[]
+a=int(input('Enter the number of items to add: '))
+for i in range(1,a+1):
+    z=input()
+    list.append(z)
+set.update(list)
+print(set)
 
 
 
